b731de4fedbe1af58199e8daf4e00676c3524ef6
puneetjyot <puneet.khurana01@nagarro.com>
2018-08-29 16:31:58 +0530

    Connecting mysql database with application using Hibernate

---
+++ src/main/java/com/nagarro/Session/RegisterDao.java
@@ -0,0 +1,23 @@
+package com.nagarro.Session;
+
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import com.nagarro.model.User;
+
+public class RegisterDao {
+
+	public void register(String username,String password){
+	SessionFactory sf=new Configuration().configure().buildSessionFactory();
+	Session session= sf.openSession();
+	session.beginTransaction();
+	User user =new User();
+	user.setUsername(username);
+	user.setPassword(password);
+	session.save(user);
+	session.getTransaction().commit(); 
+	}
+	
+	
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,24 @@
+<html>
+<head>
+<title>
+Login
+</title>
+
+
+</head>
+<body>
+<form action="RegisterServlet" method ="POST">
+
+
+UserName: <input type = "text" name = "username" required>
+         
+         <br/>
+ Password: <input type = "password" name = "password" required />
+
+<br/>
+<input type="submit" value="submit"> 
+
+</form>
+
+</body>
+</html>
